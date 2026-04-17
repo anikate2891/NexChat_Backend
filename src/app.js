9eac7d6ed1d71c6aa6c4c8ff5c8d36dfb005996e
Anikate Kanoo <anikate955@gmail.com>
2026-04-17 13:43:27 +0530

    update CORS configuration to allow multiple origins

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import morgan from "morgan";
 
 const app = express();
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://nex-chat-frontend-phi.vercel.app"],
     credentials: true
 }));
 // Middleware
